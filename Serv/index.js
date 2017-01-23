9d829c32e023b3e13bd9d91bc52a1fd5bdfa88c3
AdamDomagalsky <adadom@st.amu.edu.pl>
2017-01-23 15:25:48 +0100

    dziala

--- Serv/index.js
+++ Serv/index.js
@@ -24,17 +24,45 @@ app.get("/api/answer/:question", function(req, res){
     var question = req.params.question.toLowerCase();
  	//res.send("Powiedziałeś: "+question);
 /*moje*/
-	console.log(question)
+	console.log('Question: '+ question)
 	//console.log(sendMessage(question));
 	console.log(bot.reply('bot', question, this));
+/*
 	try {
     	bot.replyAsync("bot", question, this).then(function(reply) {
+    	console.log('Async: '+ reply)
 		res.send(reply);
     	});
 	} catch(e) {
 		console.log('WielBłąd /api/answer/:question')
 		throw new Error(e);
 	}
+*/
+
+
+
+
+	bot.replyAsync("bot", question).then(function(reply) {
+			res.send(reply)
+		}).catch(function(error){
+			console.log('WielBłąd /api/answer/:question')
+			throw new Error(e);
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
@@ -59,10 +87,16 @@ function loading_done (batch_num) {
 
     // Now the replies must be sorted!
     bot.sortReplies();
-
+/*
     // And now we're free to get a reply from the brain!
-    var reply = bot.reply("local-user", "test");
-    console.log("The bot says: " + reply);
+    var reply = bot.replyAsync("local-user", "test");
+    console.log("The bot says: " + reply);*/
+
+
+
+ bot.replyAsync("local-user", "test", this).then(function(reply) {
+   console.log("Bot> ", reply);
+});
 }
 
 // It's good to catch errors too!
@@ -70,95 +104,38 @@ function loading_error (error) {
     console.log("Error when loading files: " + error);
 }
 
-/*
-function sendMessage (input) {
-	const text = input || getText();
-	console.log('sendMsg:' + text);
-	try {
-    	bot.replyAsync("bot", text, this).then(function(reply) {
-             console.log('Odp Bota: '+reply);
-             return reply
-    	});
-
-	} catch(e) {
-		console.log('WielBłąd sendMessage')
-		throw new Error(e);
-	}
-	return 'WielBłąd sendMessage KURWAAAAAAAAAAA tutaj było udefined'
-}*/
-
-
-
-/* źle
-bot.setSubroutine("search",function(value,key,filters){
-	if (typeof(value)==='undefined') value = 'graliński';
-	if (typeof(key)==='undefined') key = 'name';
-	if (typeof(filters)==='undefined') filters = ['faculty','position','lvl','name','room','phone','mail','website','when'];
-
-	console.log('Search args: \n\tValue:' +value +'\n\tKey: '+key+'\n\tFilters: '+filters)
-*/
-
 bot.setSubroutine("search",function(rs,args){
+	return new bot.Promise(function(resolve, reject) {
+		var value = args[0];
+		var key = args[1];
+		var filters = args[2].split(",")
 
-	var value = args[0];
-	var key = args[1];
-	var filters = args[2].split(",")
+		//console.log("Args: \n\t "+typeof(key)+'\t'+key+"\n\t "+typeof(value)+'\t'+value+"\n\t "+typeof(filters)+'\t'+filters)
 
-	//console.log("Args: \n\t "+typeof(key)+'\t'+key+"\n\t "+typeof(value)+'\t'+value+"\n\t "+typeof(filters)+'\t'+filters)
+		var obj;
+		var speak;
+		fs.readFile('./wmiStaff.json', 'utf8',	function (err, data,s) {
+			var speak = ''
+			if (err) throw err;
+			dataBase = JSON.parse(data);
 
-	var speak = '';
-	var obj;
-	fs.readFile('./wmiStaff.json', 'utf8', function (err, data) {
-		if (err) throw err;
-		dataBase = JSON.parse(data);
-
-		dataBase.forEach(function(row){
-	//	console.log('Halooooooooooo'+row[key])
-			if (row[key]!='undefined' && row[key].toLowerCase().replace('-','').includes(value.replace('-',''))){
-				for( var i in filters){
-					var reply = filters[i]
-					console.log('\t'+reply+': '+row[reply])
-					speak+=' '+row[reply]
-				}
-				console.log(speak)
-				res.send(speak)
-					
-
-			}
-			
-	    });
+			dataBase.forEach(function(row){
+		//	console.log('Halooooooooooo'+row[key])
+				if (row[key]!='undefined' && row[key].toLowerCase().replace('-','').includes(value.replace('-',''))){
+					for( var i in filters){
+						var reply = filters[i]
+						//console.log('\t'+reply+': '+row[reply])
+						speak+=' '+row[reply]
+					}
+				}	
+		    });
+		    console.log('\nIn function\t\t' + speak) //11111111111
+		   //processFile(speak)
+		   resolve(speak)
+		});
 	});
 });
 
 
 
 
-/*
-function testowa(value,key,filters){
-	if (typeof(value)==='undefined') value = 'graliński';
-	if (typeof(key)==='undefined') key = 'name';
-	if (typeof(filters)==='undefined') filters = ['faculty','position','lvl','name','room','phone','mail','website','when'];
-
-	var speak = '';
-	var obj;
-	
-	fs.readFile('./wmiStaff.json', 'utf8', function (err, data) {
-		if (err) throw err;
-		dataBase = JSON.parse(data);
-
-		dataBase.forEach(function(row){
-			if (row[key].toLowerCase().replace('-','').includes(value.replace('-',''))){
-				for( var i in filters){
-					var reply = filters[i]
-					console.log('\t'+reply+': '+row[reply])
-					speak+=' '+row[reply]
-				}
-			}
-			
-	    });
-	});
-};
-
-testowa('b2-34','room',['name','room'])
-//testowa('tomasz','name',['name','lvl'])
-*/
